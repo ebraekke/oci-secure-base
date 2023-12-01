b57dff6a53412d173590c2c1656d400d39c49556
Espen Brækken <ebraekke@yahoo.com>
2023-12-01 15:39:30 +0100

    Added password(s) --2

--- main.tf
+++ main.tf
@@ -1,5 +1,6 @@
 
 
+## Vault
 module "vault" {
     source              = "./modules/vault"
 
@@ -13,8 +14,8 @@ module "secret" {
 
     compartment_ocid    = var.compartment_ocid
     display_name        = "${var.set_name}-mysql-secret"
-    description         = "${var.set_name}-mysql-secret: passowrd for MySQL"
+    description         = "${var.set_name}-mysql-secret: password for MySQL"
     vault_ocid          = module.vault.vault_ocid
-    content             = local.mysql_password
+    content             = base64encode(local.mysql_password)
     key_ocid            = module.vault.vault_master_key_ocid
 }
